96dba6e42a5dce0546de520f4f2c18b10351807a
Simone Conigliaro <sconigliaro@spartaglobal.com>
2022-04-07 17:48:35 +0100

    added news methods

--- src/test/java/com/sparta/mm/pom/HackerNewsPOMTests.java
+++ src/test/java/com/sparta/mm/pom/HackerNewsPOMTests.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class HackerNewsPOMTests {
     private static WebDriver driver;
     private HNHomepage homepage;
 
+    private static final String[] headers = new String[]{"Hacker News", "new", "past", "comments", "ask", "show", "jobs", "submit"};
+
     @BeforeAll
-    static void setupAll(){
+    static void setupAll() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
     }
 
@@ -19,5 +22,27 @@ public class HackerNewsPOMTests {
         driver = new ChromeDriver();
         homepage = new HNHomepage(driver);
     }
+
+    @Test
+    @DisplayName("check headers exist")
+    void checkHeadersExists() {
+        for (String header : headers) {
+            Assertions.assertTrue(homepage.doesHeaderExist(header));
+        }
+    }
+
+    @Test
+    @DisplayName("check number articles is 30")
+    void checkNumberArticlesIs30() {
+        Assertions.assertEquals(30, homepage.getNumberOfArticles());
+    }
+
+    @Test
+    @DisplayName("check if 30 new articles are shown")
+    void checkIf30NewArticlesAreShown() {
+        homepage.loadMoreArticles();
+        Assertions.assertTrue((homepage.getNumberArticle(1) == 31) && (homepage.getNumberArticle(30) == 60));
+    }
+
 
 }
