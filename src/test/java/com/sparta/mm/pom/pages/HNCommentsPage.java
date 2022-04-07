1981a64594533b62cc26d1d2bea97e8e0a199dfa
Simone Conigliaro <sconigliaro@spartaglobal.com>
2022-04-07 15:52:14 +0100

    added structure

---
+++ src/test/java/com/sparta/mm/pom/pages/HNCommentsPage.java
@@ -0,0 +1,13 @@
+package com.sparta.mm.pom.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HNCommentsPage {
+
+    private WebDriver driver;
+
+    public HNCommentsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
