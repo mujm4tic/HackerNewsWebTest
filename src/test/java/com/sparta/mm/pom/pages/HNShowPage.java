b27ace9ed5effee7f1d05e52820153f1a1f8f68f
unknown <wleung@spartaglobal.com>
2022-04-08 14:24:14 +0100

    Update HN show page

--- src/test/java/com/sparta/mm/pom/pages/HNShowPage.java
+++ src/test/java/com/sparta/mm/pom/pages/HNShowPage.java
@@ -15,11 +15,56 @@ public class HNShowPage {
         return count == 30;
     }
 
+    public boolean isTitleStartWithShowHN() {
+        boolean titleStartWithShowHN = true;
+        int numberOfArticle = getNumberOfArticle();
+        String[] articles = new String[numberOfArticle];
+        for (int i = 0; i < numberOfArticle; i ++) {
+            articles[i] = getTitleOfTheArticle(i);
+            if (!articles[i].startsWith("Show HN:"))
+                titleStartWithShowHN = false;
+        }
+        return titleStartWithShowHN;
+    }
+
+    public boolean isTitleAlwaysStartWithShowHN() {
+        boolean isMoreExist = isMoreExist();
+        boolean titleStartWithShowHN = true;
+        while (isMoreExist) {
+            int numberOfArticle = getNumberOfArticle();
+            String[] articles = new String[numberOfArticle];
+            for (int i = 0; i < numberOfArticle; i ++) {
+                articles[i] = getTitleOfTheArticle(i);
+                if (!articles[i].startsWith("Show HN:"))
+                    titleStartWithShowHN = false;
+            }
+            clickMore();
+            isMoreExist = isMoreExist();
+        }
+        return titleStartWithShowHN;
+    }
+
+    public String getTitleOfTheArticle(int i) {
+        return driver.findElements(By.className("titlelink")).get(i).getText();
+    }
+
     public boolean isThatTheShowLinkWorks() {
         return getUrl().equals("https://news.ycombinator.com/show");
     }
 
     private String getUrl() {
         return driver.getCurrentUrl();
+    }
+
+    private int getNumberOfArticle() {
+        return driver.findElements(By.className("titlelink")).size();
+    }
+
+    private void clickMore() {
+        driver.findElement(By.className("morelink")).click();
+    }
+
+    private boolean isMoreExist() {
+        return driver.findElements(By.className("morelink")).size() > 0;
     }
 }
